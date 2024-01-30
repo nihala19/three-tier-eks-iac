91367521c40f5c1da09a3697eb64357dd548852f
Nihal Prakash Apretwar <120296751+nihala19@users.noreply.github.com>
2024-01-30 16:04:21 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "sandip-demo-tfstate-bucket"
+    bucket = "nihal-demo-tfstate-bucket"
     key    = "eks/terraform.tfstate"
     region = "us-west-2"
   }
